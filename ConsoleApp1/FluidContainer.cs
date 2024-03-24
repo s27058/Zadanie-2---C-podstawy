66299f8d14293a6a242c36e9746077d809afe3fe
s27058@pjwstk.edu.pl <s27058@pjwstk.edu.pl>
2024-03-24 16:37:20 +0100

    Changed gitignore

---
+++ ConsoleApp1/FluidContainer.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsoleApp1;
+
+public class FluidContainer : Container, IHazardNotifier
+{
+    public bool IsFluidDangerous { get; set; }
+    public string? FluidName { get; set; }
+    public FluidContainer(int height, int weight, int depth, double maxCargoWeight) : base(height, weight, depth, maxCargoWeight)
+    {
+        SerialString = "KON-L-" + CurrentSerialNumber;
+        CurrentSerialNumber++;
+    }
+
+    public void NotifyDangerousSituation()
+    {
+        Console.WriteLine($"W kontenerze o numerze seryjnym: {SerialString}, zaszła niebezpieczna sytuacja.");
+    }
+
+    public override void EmptyContainer()
+    {
+        base.EmptyContainer();
+        FluidName = null;
+    }
+    
+    public void LoadContainer(double loadWeight, bool isFluidDangerous, string fluidName)
+    {
+        if (isFluidDangerous)
+        {
+            if (CargoWeight + loadWeight > MaxCargoWeight*0.5)
+            {
+                NotifyDangerousSituation();
+                return;
+            }
+        }
+        else
+        {
+            if (CargoWeight + loadWeight > MaxCargoWeight*0.9)
+            {
+                NotifyDangerousSituation();
+                return;
+            }
+        }
+
+        FluidName = fluidName;
+        IsFluidDangerous = isFluidDangerous;
+        CargoWeight += loadWeight;
+    }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Kontener na płyny");
+        base.PrintInfo();
+        if (FluidName != null)
+        {
+            Console.WriteLine("Nazwa przewożonego płynu: " + FluidName);
+            Console.WriteLine("Czy przewożony płyn jest niebezpieczny: " + IsFluidDangerous);
+        }
+        else
+        {
+            Console.WriteLine("Kontener jest pusty");
+        }
+        
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var container1 = new FluidContainer(10, 20, 10, 100);
+        container1.LoadContainer(40, true, "metanol");
+        var container2 = new GasContainer(8, 15, 12, 50, 0);
+        container2.LoadContainer(30);
+        var container3 = new FreezeContainer(8, 15, 12, 50, "banany", 13.3, 15);
+        container3.LoadContainer(45, "banany");
+        container3.PrintInfo();
+        
+        var containers = new List<Container>();
+        containers.Add(container2);
+        containers.Add(container3);
+        
+        var containerShip = new ContainerShip(10, 100, 30000);
+        containerShip.LoadContainer(container1);
+        containerShip.LoadContainers(containers);
+        containerShip.UnloadContainer(container3);
+        container3.EmptyContainer();
+        containerShip.ReplaceContainer(container2, container3);
+        var containerShip2 = new ContainerShip(15, 50, 15000);
+        containerShip.MoveContainer(containerShip2, container3);
+        containerShip.PrintInfo();
+        
+    }
+}
